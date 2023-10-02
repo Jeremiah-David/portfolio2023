ae1b4195dde1923b4a3982795d46821eac78278c
Jeremiah J David <Dancesfire@gmail.com>
2023-10-01 19:56:04 -0700

    fixed build

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,27 +2,15 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import NavBar from "./Navbar";
-// import headshot from ";
 
-import Image from "next/image";
 
 export default function HeroSection() {
   return (
     <>
-      <NavBar />
-      <Container fluid className="hero-section p-4">
-        <Row className="">
-
-
-          <Col
-            className=" "
-            md={7}
-          >
-            <h4 className="  ">
+            <h4>
               Welcome Friend
             </h4>
-            <h4 className="">
-              I'm Jeremiah. I build websites and web applications, teach and
+            <h4> Hi! My name is Jeremiah. I build websites and web applications, teach and
               mentor, and offer a variety of web based solutions.
             </h4>
             <a href="#contact">
@@ -30,10 +18,10 @@ export default function HeroSection() {
                 Contact Me
               </button>
             </a>
-          </Col>
+ 
        
-        </Row>
-      </Container>
+
+
     </>
   );
 }
